6b824c45a040f00ba1b03d4e61eb1cc1846a4ad5
Yogi Spice <caseynnewman@yahoo.com>
2024-01-05 16:01:28 -0500

    added event listener to search button

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,16 @@
-
+const searchBtn = document.querySelector('#search-button');
+const input = document.querySelector('#city-search');
 
 
 
 const getWeather = async (url) => {
-    const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?appid=fe48577d7995f2974587723e4b533c3c')
+    let inputValue = input.value;
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?${inputValue}&appid=fe48577d7995f2974587723e4b533c3c`)
     const data = await response.json()
     console.log(data)
 }
+
+
+
+searchBtn.addEventListener('click', getWeather)
