bc24bf0c2e1a835d9cf27bd4d2c0afd5b761954c
Yogi Spice <caseynnewman@yahoo.com>
2024-01-06 23:31:09 -0500

    adding getCityName and displayCurrentWeather functions

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,29 @@
 const searchBtn = document.querySelector('#search-button');
 const cityInput = document.querySelector('#city-search');
 const currentWeatherDiv = document.querySelector('#current-weather');
+let currentCity;
+let cityName = [];
 
 
 const getWeather = async (url) => {
-    let currentCity = cityInput.value;
+    currentCity = cityInput.value;
 
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=fe48577d7995f2974587723e4b533c3c`)
     const data = await response.json()
     console.log(data)
 }
 
+const getCityName = () => {
+    cityName[index] = currentCity.name;
+}
+
+getCityName()
+
 const displayCurrentWeather = () => {
     let currentCityDisplay = document.createElement('h2');
-    currentCityDisplay.textContent = cityInput.value;
+    currentCityDisplay.textContent = currentCity;
+    console.log(currentCity)
+
     currentWeatherDiv.appendChild(currentCityDisplay);
 }
 
