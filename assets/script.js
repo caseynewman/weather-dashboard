ffca498bf9d171354e51265e5ee608c1a9a3151b
Yogi Spice <caseynnewman@yahoo.com>
2024-01-06 23:11:02 -0500

    added style to divs

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,23 @@
 const searchBtn = document.querySelector('#search-button');
-const input = document.querySelector('#city-search');
-
+const cityInput = document.querySelector('#city-search');
+const currentWeatherDiv = document.querySelector('#current-weather');
 
 
 const getWeather = async (url) => {
-    let inputValue = input.value;
+    let currentCity = cityInput.value;
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=fe48577d7995f2974587723e4b533c3c`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=fe48577d7995f2974587723e4b533c3c`)
     const data = await response.json()
     console.log(data)
 }
 
 const displayCurrentWeather = () => {
-    
+    let currentCityDisplay = document.createElement('h2');
+    currentCityDisplay.textContent = cityInput.value;
+    currentWeatherDiv.appendChild(currentCityDisplay);
 }
 
-
+displayCurrentWeather()
 
 
 
