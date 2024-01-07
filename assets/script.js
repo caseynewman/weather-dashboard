1a585749274cee2992df14be8423d319d8bf3a13
Yogi Spice <caseynnewman@yahoo.com>
2024-01-06 22:10:40 -0500

    added URL for current weather by city

--- assets/script.js
+++ assets/script.js
@@ -6,10 +6,17 @@ const input = document.querySelector('#city-search');
 const getWeather = async (url) => {
     let inputValue = input.value;
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?${inputValue}&appid=fe48577d7995f2974587723e4b533c3c`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=fe48577d7995f2974587723e4b533c3c`)
     const data = await response.json()
     console.log(data)
 }
+
+const displayCurrentWeather = () => {
+    
+}
+
+
+
 
 
 
