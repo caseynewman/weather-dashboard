c9c075c941ffd32cab25b2a45f4c5d18214c323c
Yogi Spice <caseynnewman@yahoo.com>
2024-01-07 20:37:50 -0500

    appended city name to page

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,27 @@
 const searchBtn = document.querySelector('#search-button');
 const cityInput = document.querySelector('#city-search');
 const currentWeatherDiv = document.querySelector('#current-weather');
-let currentCity;
-let cityName = [];
+let cityName;
 
 
 const getWeather = async (url) => {
-    currentCity = cityInput.value;
+    let currentCity = cityInput.value;
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=fe48577d7995f2974587723e4b533c3c`)
-    const data = await response.json()
-    console.log(data)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=fe48577d7995f2974587723e4b533c3c`);
+    const data = await response.json();
+    cityName = data.name;
+
+    displayCurrentWeather()
 }
-
-const getCityName = () => {
-    cityName[index] = currentCity.name;
-}
-
-getCityName()
 
 const displayCurrentWeather = () => {
-    let currentCityDisplay = document.createElement('h2');
-    currentCityDisplay.textContent = currentCity;
-    console.log(currentCity)
-
+    const currentCityDisplay = document.createElement('h2');
+    currentCityDisplay.textContent = cityName;
     currentWeatherDiv.appendChild(currentCityDisplay);
+
+
+
 }
-
-displayCurrentWeather()
-
 
 
 
